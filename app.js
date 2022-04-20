5dcda66f9e7a7f69b98feffb40507978a239abcc
hammackjg <hammackjg@g.crookcountyschools.org>
2022-04-20 10:08:48 -0700

    modified app.js

--- app.js
+++ app.js
@@ -8,6 +8,11 @@ const picContainerElement = document.getElementById('image1title');
 const picContainerElement = document.getElementById('image2title');
 const picContainerElement = document.getElementById('image3title');
 
+let count=0
+
+let pic1 = null;
+let pic2 = null;
+let pic3 = null; 
 function Image(name, imgPath,){
     this.name = name;
     this.imgPath = imgPath;
@@ -16,6 +21,12 @@ function Image(name, imgPath,){
 }
 
 Image.allImages = []
+
+Image.prototype.renderImage = funtction(img,h2); {
+    img.src = this.imgPath;
+    h2.textContent = thid.name;
+    this.views ++;
+}
 
 Image.allImages.push(new Image('Banana' , './images/banana.jpg'));
 Image.allImages.push(new Image('Bathroom' , './images/bathroom.jpg'));
@@ -35,5 +46,30 @@ Image.allImages.push(new Image('Dragon' , './images/dragon.jpg'));
 Image.allImages.push(new Image('Chair' , './images/chair.jpg'));
 Image.allImages.push(new Image('Cthulhu' , './images/cthulhu.jpg'));
 Image.allImages.push(new Image('Bag' , './images/bag.jpg'));
+
+function getThreeImages(){
+    const cantUse = [pic1, pic2, pic3];
+    if(cantUse.inlcludes(pic1)){
+        let picOne = Math.floor(Math.random() * Image.allImages.length);
+        pic1= Image.allImages[picOne];
+    }
+    cantUse.push[pic1]
+    if(cantUse.includes(pic2)){
+        let picTwo = Math.floor(Math.random() * Image.allImages.lenght);
+        pic2 = Image.allImages[picTwo];
+    }
+    cantUse.push[pic2]
+    if(cantUse.includes(pic3)){
+        let picThree = Math.floor(Math.random()* Image.allImages.lenght);
+        pic3 = Image.allImages[picThree];
+    }
+
+    function renderImage(){
+        pic1.renderImage(firstPicElement, firstPicTitle);
+        pic2.renderImage(secondPicElement, secondPicTitle);
+        pic3.renderImage(thirdPicElement, thirdPicTitle);
+    }
+}
+
 
 
